aea50f035c4e577b835c604c2c09f8bed223dc30
i4mitty <126401596+i4mitty@users.noreply.github.com>
2023-03-01 10:28:19 -0500

    Create Alert.lua

---
+++ Alert.lua
@@ -0,0 +1,12 @@
+local text   = ""
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+local function say() 
+Event:FireServer(text, "All")
+end
+text = "⚫⚫⚫⚫⚫⚫" say() 
+text = "⚫🔴🔴🔴🔴⚫" say() 
+text = "⚫⚫🔴🔴⚫⚫" say() 
+text = "⚫⚫🔴🔴⚫⚫" say() 
+text = "⚫🔴🔴🔴🔴⚫" say() 
+text = "⚫⚫⚫⚫⚫⚫" say() 
+text = "InfernoHub [i4mitty♯3554] [Inferno]: DS Invite - [9ATH7DPEgT]" say()

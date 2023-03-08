23126faaf6b4b72017e40e2992538674dbe6464b
i4mitty <126401596+i4mitty@users.noreply.github.com>
2023-03-08 18:45:33 -0500

    Update Anti Fling Inferno.lua

--- Anti Fling Inferno.lua
+++ Anti Fling Inferno.lua
@@ -3,18 +3,18 @@ local players = game:GetService("Players")
 local plr = players.LocalPlayer
 
 RunService.Stepped:Connect(function()
-    for i, CoPlayer in pairs(players:GetChildren()) do
-        if CoPlayer ~= plr and CoPlayer.Character then
-            for i,v1 in pairs(CoPlayer.Character:GetChildren()) do
-                if v1.ClassName == "MeshPart" or v1.Name == "HumanoidRootPart" then
-                    v1.CanCollide = false
+	for i, CoPlayer in pairs(players:GetChildren()) do
+		if CoPlayer ~= plr and CoPlayer.Character then
+			for i,v1 in pairs(CoPlayer.Character:GetChildren()) do
+				if v1.ClassName == "MeshPart" or v1.Name == "HumanoidRootPart" then
+					v1.CanCollide = false
 					v1.AssemblyAngularVelocity = Vector3.new(0, 0, 0)
 					v1.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
 					v1.CustomPhysicalProperties = PhysicalProperties.new(0, 0, 0)
-                elseif v1:IsA("Accessory") and v1:FindFirstChildOfClass("MeshPart") then
-                    v1:FindFirstChildOfClass("MeshPart").CanCollide = false
-                end
-            end
-        end
-    end
+				elseif v1:IsA("Accessory") then
+					v1.Handle.CanCollide = false
+				end
+			end
+		end
+	end
 end)

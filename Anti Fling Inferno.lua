7c613dce75e56ce71a083a69f89db022b9cd511a
i4mitty <126401596+i4mitty@users.noreply.github.com>
2023-03-09 19:43:08 -0500

    Update Anti Fling Inferno.lua

--- Anti Fling Inferno.lua
+++ Anti Fling Inferno.lua
@@ -6,7 +6,7 @@ RunService.Stepped:Connect(function()
 	for i, CoPlayer in pairs(players:GetChildren()) do
 		if CoPlayer ~= plr and CoPlayer.Character then
 			for i,v1 in pairs(CoPlayer.Character:GetChildren()) do
-				if v1.ClassName == "MeshPart" or v1.Name == "HumanoidRootPart" then
+				if v1.ClassName == "MeshPart" or v1:IsA("Part") or v1.Name == "HumanoidRootPart" then
 					v1.CanCollide = false
 					v1.AssemblyAngularVelocity = Vector3.new(0, 0, 0)
 					v1.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
